1bfc0097bc9380799557307ba37759a43d6f99eb
Sven Panne <svenpanne@gmail.com>
2013-08-23 14:26:53 +0200

    Added support for ARB_occlusion_query2.

---
+++ src/Graphics/Rendering/OpenGL/Raw/ARB/OcclusionQuery2.hs
@@ -0,0 +1,24 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Graphics.Rendering.OpenGL.Raw.ARB.OcclusionQuery2
+-- Copyright   :  (c) Sven Panne 2013
+-- License     :  BSD3
+--
+-- Maintainer  :  Sven Panne <svenpanne@gmail.com>
+-- Stability   :  stable
+-- Portability :  portable
+--
+-- All raw functions and tokens from the ARB_occlusion_query2 extension, see
+-- <http://www.opengl.org/registry/specs/ARB/occlusion_query2.txt>.
+--
+--------------------------------------------------------------------------------
+
+module Graphics.Rendering.OpenGL.Raw.ARB.OcclusionQuery2 (
+   -- * Tokens
+   gl_ANY_SAMPLES_PASSED
+) where
+
+import Graphics.Rendering.OpenGL.Raw.Core31.Types
+
+gl_ANY_SAMPLES_PASSED :: GLenum
+gl_ANY_SAMPLES_PASSED = 0x8C2F
