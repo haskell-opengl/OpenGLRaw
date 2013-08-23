1568296be8d444b26c4c34ba61e49a9ea36ec8f4
Sven Panne <svenpanne@gmail.com>
2013-08-23 09:44:27 +0200

    Use OPTIONS_HADDOCK instead of #hide.

--- src/Graphics/Rendering/OpenGL/Raw/ARB/Compatibility/Functions.hs
+++ src/Graphics/Rendering/OpenGL/Raw/ARB/Compatibility/Functions.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE ForeignFunctionInterface, CPP #-}
--- #hide
+{-# OPTIONS_HADDOCK hide #-}
 --------------------------------------------------------------------------------
 -- |
 -- Module      :  Graphics.Rendering.OpenGL.Raw.ARB.Compatibility.Functions
