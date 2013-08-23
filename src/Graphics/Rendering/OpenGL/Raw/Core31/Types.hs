02c53c3fe392fc821d57c3e9f7cfd559ba8f5f05
Sven Panne <svenpanne@gmail.com>
2013-08-23 10:27:09 +0200

    Added comments to OpenGL types.

--- src/Graphics/Rendering/OpenGL/Raw/Core31/Types.hs
+++ src/Graphics/Rendering/OpenGL/Raw/Core31/Types.hs
@@ -8,8 +8,8 @@
 -- Stability   :  stable
 -- Portability :  portable
 --
--- All types from the OpenGL 3.1 core plus some later ones, see
--- <http://www.opengl.org/registry/>.
+-- This module corresponds to table 2.2 in section 2.2 (Command Syntax) of the
+-- OpenGL 4.4 specs.
 --
 --------------------------------------------------------------------------------
 
@@ -20,26 +20,71 @@ import Data.Word
 import Foreign.C.Types
 import Foreign.Ptr
 
+--------------------------------------------------------------------------------
 
-type GLboolean  = CUChar
-type GLbyte     = CSChar
-type GLubyte    = CUChar
-type GLchar     = CChar
-type GLshort    = CShort
-type GLushort   = CUShort
-type GLint      = CInt
-type GLuint     = CUInt
-type GLfixed    = CInt     -- Introduced in 4.1.
-type GLint64    = Int64    -- Introduced in 3.2.
-type GLuint64   = Word64   -- Introduced in 3.2.
-type GLsizei    = CInt
-type GLenum     = CUInt
-type GLintptr   = CPtrdiff
+-- | 1bit boolean.
+type GLboolean = CUChar
+
+-- | 8bit signed two\'s complement binary integer.
+type GLbyte = CSChar
+
+-- | 8bit unsigned binary integer.
+type GLubyte = CUChar
+
+-- | 8bit characters making up strings.
+type GLchar = CChar
+
+-- | 16bit signed two\'s complement binary integer.
+type GLshort = CShort
+
+-- | 16bit unsigned binary integer.
+type GLushort = CUShort
+
+-- | 32bit signed two\'s complement binary integer.
+type GLint = CInt
+
+-- | 32bit unsigned binary integer.
+type GLuint = CUInt
+
+-- | 32bit signed two\'s complement 16.16 scaled integer (introduced in OpenGL
+-- 4.1).
+type GLfixed = CInt
+
+-- | 64bit signed two\'s complement binary integer (introduced in OpenGL 3.2).
+type GLint64 = Int64
+
+-- | 64bit unsigned binary integer (introduced in OpenGL 3.2).
+type GLuint64 = Word64
+
+-- | 32bit non-negative binary integer size.
+type GLsizei = CInt
+
+-- | 32bit enumerated binary integer value.
+type GLenum = CUInt
+
+-- | Pointer-sized signed two\'s complement binary integer.
+type GLintptr = CPtrdiff
+
+-- | Pointer-sized non-negative binary integer size.
 type GLsizeiptr = CPtrdiff
-type GLsync     = Ptr ()   -- Introduced in 3.2.
+
+-- | Pointer-sized sync object handle (introduced in OpenGL 3.2).
+type GLsync = Ptr ()
+
+-- | 32bit bit field.
 type GLbitfield = CUInt
-type GLhalf     = CUShort
-type GLfloat    = CFloat
-type GLclampf   = CFloat   -- No longer used in 4.3.
-type GLdouble   = CDouble
-type GLclampd   = CDouble  -- No longer used in 4.3
+
+-- | 16bit half-precision floating-point value encoded in an unsigned scalar.
+type GLhalf = CUShort
+
+-- | 32bit floating-point value.
+type GLfloat = CFloat
+
+-- | 32bit floating-point value clamped to [0, 1] (no longer used in OpenGL 4.3).
+type GLclampf = CFloat
+
+-- | 64bit floating-point value.
+type GLdouble = CDouble
+
+-- | 64bit floating-point value clamped to [0, 1]  (no longer used in OpenGL 4.3).
+type GLclampd = CDouble
