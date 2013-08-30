230b7a154a608f5d0a110fb96d2f7d79ef910f01
Sven Panne <svenpanne@gmail.com>
2013-08-30 11:07:42 +0200

    Added support for ARB_compute_shader.

--- src/Graphics/Rendering/OpenGL/Raw/ARB.hs
+++ src/Graphics/Rendering/OpenGL/Raw/ARB.hs
@@ -89,6 +89,7 @@ module Graphics.Rendering.OpenGL.Raw.ARB (
    module Graphics.Rendering.OpenGL.Raw.ARB.TransformFeedback3,          -- #94
    module Graphics.Rendering.OpenGL.Raw.ARB.ES2Compatibility,            -- #95
    module Graphics.Rendering.OpenGL.Raw.ARB.ShaderAtomicCounters,        -- #114
+   module Graphics.Rendering.OpenGL.Raw.ARB.ComputeShader,               -- #122
    module Graphics.Rendering.OpenGL.Raw.ARB.ES3Compatibility,            -- #127
    module Graphics.Rendering.OpenGL.Raw.ARB.ShaderStorageBufferObject,   -- #137
    module Graphics.Rendering.OpenGL.Raw.ARB.QueryBufferObject            -- #148
@@ -165,6 +166,7 @@ import Graphics.Rendering.OpenGL.Raw.ARB.DrawIndirect
 import Graphics.Rendering.OpenGL.Raw.ARB.TransformFeedback3
 import Graphics.Rendering.OpenGL.Raw.ARB.ES2Compatibility
 import Graphics.Rendering.OpenGL.Raw.ARB.ShaderAtomicCounters
+import Graphics.Rendering.OpenGL.Raw.ARB.ComputeShader
 import Graphics.Rendering.OpenGL.Raw.ARB.ES3Compatibility
 import Graphics.Rendering.OpenGL.Raw.ARB.ShaderStorageBufferObject
 import Graphics.Rendering.OpenGL.Raw.ARB.QueryBufferObject
