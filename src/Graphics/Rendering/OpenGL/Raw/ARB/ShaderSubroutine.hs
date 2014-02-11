b6b93005c96b904cc7354da7ecebcad1165e2c21
Sven Panne <svenpanne@gmail.com>
2014-02-11 15:00:22 +0100

    Added missing file.

---
+++ src/Graphics/Rendering/OpenGL/Raw/ARB/ShaderSubroutine.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE ForeignFunctionInterface, CPP #-}
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Graphics.Rendering.OpenGL.Raw.ARB.ShaderSubroutine
+-- Copyright   :  (c) Sven Panne 2014
+-- License     :  BSD3
+--
+-- Maintainer  :  Sven Panne <svenpanne@gmail.com>
+-- Stability   :  stable
+-- Portability :  portable
+--
+-- All raw functions and tokens from the ARB_shader_subroutine extension, see
+-- <http://www.opengl.org/registry/specs/ARB/shader_subroutine.txt>.
+--
+--------------------------------------------------------------------------------
+
+module Graphics.Rendering.OpenGL.Raw.ARB.ShaderSubroutine (
+   -- * Functions
+   glGetSubroutineUniformLocation,
+   glGetSubroutineIndex,
+   glGetActiveSubroutineUniformiv,
+   glGetActiveSubroutineUniformName,
+   glGetActiveSubroutineName,
+   glUniformSubroutinesuiv,
+   glGetUniformSubroutineuiv,
+   glGetProgramStageiv,
+
+   -- * Tokens
+   gl_ACTIVE_SUBROUTINES,
+   gl_ACTIVE_SUBROUTINE_UNIFORMS,
+   gl_ACTIVE_SUBROUTINE_UNIFORM_LOCATIONS,
+   gl_ACTIVE_SUBROUTINE_MAX_LENGTH,
+   gl_ACTIVE_SUBROUTINE_UNIFORM_MAX_LENGTH,
+   gl_MAX_SUBROUTINES,
+   gl_MAX_SUBROUTINE_UNIFORM_LOCATIONS,
+   gl_NUM_COMPATIBLE_SUBROUTINES,
+   gl_COMPATIBLE_SUBROUTINES,
+   gl_UNIFORM_SIZE,
+   gl_UNIFORM_NAME_LENGTH
+) where
+
+import Foreign.C.Types
+import Foreign.Ptr
+import Graphics.Rendering.OpenGL.Raw.ARB.UniformBufferObject
+import Graphics.Rendering.OpenGL.Raw.Core31.Types
+import Graphics.Rendering.OpenGL.Raw.Extensions
+
+--------------------------------------------------------------------------------
+
+#include "HsOpenGLRaw.h"
+
+extensionNameString :: String
+extensionNameString = "GL_ARB_shader_subroutine"
+
+EXTENSION_ENTRY(glGetSubroutineUniformLocation,GLuint -> GLenum -> Ptr GLchar -> IO GLint)
+EXTENSION_ENTRY(glGetSubroutineIndex,GLuint -> GLenum -> Ptr GLchar -> IO GLuint)
+EXTENSION_ENTRY(glGetActiveSubroutineUniformiv,GLuint -> GLenum -> GLuint -> GLenum -> Ptr GLint -> IO ())
+EXTENSION_ENTRY(glGetActiveSubroutineUniformName,GLuint -> GLenum -> GLuint -> GLsizei -> Ptr GLsizei -> Ptr GLchar -> IO ())
+EXTENSION_ENTRY(glGetActiveSubroutineName,GLuint -> GLenum -> GLuint -> GLsizei -> Ptr GLsizei -> Ptr GLchar -> IO ())
+EXTENSION_ENTRY(glUniformSubroutinesuiv,GLenum -> GLsizei -> Ptr GLuint -> IO ())
+EXTENSION_ENTRY(glGetUniformSubroutineuiv,GLenum -> GLint -> Ptr GLuint -> IO ())
+EXTENSION_ENTRY(glGetProgramStageiv,GLuint -> GLenum -> GLenum -> Ptr GLint -> IO ())
+
+gl_ACTIVE_SUBROUTINES :: GLenum
+gl_ACTIVE_SUBROUTINES = 0x8DE5
+
+gl_ACTIVE_SUBROUTINE_UNIFORMS :: GLenum
+gl_ACTIVE_SUBROUTINE_UNIFORMS = 0x8DE6
+
+gl_ACTIVE_SUBROUTINE_UNIFORM_LOCATIONS :: GLenum
+gl_ACTIVE_SUBROUTINE_UNIFORM_LOCATIONS = 0x8E47
+
+gl_ACTIVE_SUBROUTINE_MAX_LENGTH :: GLenum
+gl_ACTIVE_SUBROUTINE_MAX_LENGTH = 0x8E48
+
+gl_ACTIVE_SUBROUTINE_UNIFORM_MAX_LENGTH :: GLenum
+gl_ACTIVE_SUBROUTINE_UNIFORM_MAX_LENGTH = 0x8E49
+
+gl_MAX_SUBROUTINES :: GLenum
+gl_MAX_SUBROUTINES = 0x8DE7
+
+gl_MAX_SUBROUTINE_UNIFORM_LOCATIONS :: GLenum
+gl_MAX_SUBROUTINE_UNIFORM_LOCATIONS = 0x8DE8
+
+gl_NUM_COMPATIBLE_SUBROUTINES :: GLenum
+gl_NUM_COMPATIBLE_SUBROUTINES = 0x8E4A
+
+gl_COMPATIBLE_SUBROUTINES :: GLenum
+gl_COMPATIBLE_SUBROUTINES = 0x8E4B
