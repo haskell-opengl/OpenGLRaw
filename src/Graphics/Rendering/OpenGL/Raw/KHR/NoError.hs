e9686986762acdef062a8551180c75b7cf9ce90b
Sven Panne <svenpanne@gmail.com>
2015-05-28 20:38:47 +0200

    Updated OpenGL registry to r31191. Bumped version to 2.5.1.0.

---
+++ src/Graphics/Rendering/OpenGL/Raw/KHR/NoError.hs
@@ -0,0 +1,20 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Graphics.Rendering.OpenGL.Raw.KHR.NoError
+-- Copyright   :  (c) Sven Panne 2015
+-- License     :  BSD3
+--
+-- Maintainer  :  Sven Panne <svenpanne@gmail.com>
+-- Stability   :  stable
+-- Portability :  portable
+--
+-- The <https://www.opengl.org/registry/specs/KHR/no_error.txt KHR_no_error> extension.
+--
+--------------------------------------------------------------------------------
+
+module Graphics.Rendering.OpenGL.Raw.KHR.NoError (
+  -- * Enums
+  gl_CONTEXT_FLAG_NO_ERROR_BIT_KHR
+) where
+
+import Graphics.Rendering.OpenGL.Raw.Tokens
