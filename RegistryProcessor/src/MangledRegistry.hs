24f6fc67affb8e990228c9f03b28275e47187a7b
Sven Panne <svenpanne@gmail.com>
2016-01-01 19:51:25 +0100

    Removed redundant import.

--- RegistryProcessor/src/MangledRegistry.hs
+++ RegistryProcessor/src/MangledRegistry.hs
@@ -27,7 +27,6 @@ module MangledRegistry (
   splitWords, joinWords
 ) where
 
-import qualified Data.Char as C
 import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Maybe as DM
